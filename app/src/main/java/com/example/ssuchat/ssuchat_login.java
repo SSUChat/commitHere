d9cbba8a8d5862d7fca0ba66b58b9ffba96fd4a5
kimdoyun <roger428@naver.com>
2023-11-13 21:52:37 +0900

    Doyun Kim made

--- app/src/main/java/com/example/ssuchat/ssuchat_login.java
+++ app/src/main/java/com/example/ssuchat/ssuchat_login.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.ssuchat.databinding.ActivitySsuchatLoginBinding;
+
 public class ssuchat_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ssuchat_login);
+
+        ActivitySsuchatLoginBinding binding = ActivitySsuchatLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
     }
 }
--- app/src/main/java/com/example/ssuchat/ssuchat_register.java
+++ app/src/main/java/com/example/ssuchat/ssuchat_register.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.ssuchat.databinding.ActivitySsuchatRegisterBinding;
+import com.google.firebase.ktx.Firebase;
+
 public class ssuchat_register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ssuchat_register);
+
+        ActivitySsuchatRegisterBinding binding = ActivitySsuchatRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
