086229b0031de51d7d453fe4661c3916d9124edd
kimdoyun <roger428@naver.com>
2023-11-18 14:55:31 +0900

    Doyun Kim made

--- app/src/main/java/com/example/ssuchat/ssuchat_register.java
+++ app/src/main/java/com/example/ssuchat/ssuchat_register.java
@@ -54,6 +54,15 @@ public class ssuchat_register extends AppCompatActivity {
 
     }
 
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            updateUI(currentUser);
+        }
+    }
+
     private void signUp(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
