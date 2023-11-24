8758eb9d21aa691b64fcf3815eb916fc1a3faefb
kimdoyun <roger428@naver.com>
2023-11-24 20:21:06 +0900

    Doyun Kim made

--- app/src/main/java/com/example/ssuchat/MainActivity.java
+++ app/src/main/java/com/example/ssuchat/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ssuchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ssuchat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+//        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
+//            }
+//        });
+
     }
 }
