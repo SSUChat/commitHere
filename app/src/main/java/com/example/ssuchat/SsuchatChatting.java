98e9e13dcf0aa41f4539ea23b0e86d8a8c3c36a5
kimdoyun <roger428@naver.com>
2023-11-27 19:31:39 +0900

    Doyun Kim made

---
+++ app/src/main/java/com/example/ssuchat/SsuchatChatting.java
@@ -0,0 +1,14 @@
+package com.example.ssuchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SsuchatChatting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ssuchat_chatting);
+    }
+}
