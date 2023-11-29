6f514a34c1589c9c777df528a4ce0b9afb7de243
yang5864 <yang5864@naver.com>
2023-11-29 22:07:52 +0900

    YSH

--- app/src/main/java/com/example/ssuchat/ssuchat_register.java
+++ app/src/main/java/com/example/ssuchat/ssuchat_register.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.ktx.Firebase;
 
 public class ssuchat_register extends AppCompatActivity {
@@ -38,15 +40,23 @@ public class ssuchat_register extends AppCompatActivity {
 
         initFirebaseAuth();
 
+        EditText editName = binding.editName;
         EditText editEmail = binding.editEmail;
         EditText editPassword = binding.editPassword;
+        EditText editStudentId = binding.editStudentId;
 
         binding.buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = editName.getText().toString();
                 String email = editEmail.getText().toString();
                 String password = editPassword.getText().toString();
+                String studentId = editStudentId.getText().toString();
 
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(studentId)) {
+                    Toast.makeText(ssuchat_register.this, "모든 필드를 입력하세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 signUp(email, password);
             }
         });
@@ -60,7 +70,15 @@ public class ssuchat_register extends AppCompatActivity {
             }
         });
 
-    }
+        binding.goLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 취소 버튼을 눌렀을 때 ssuchat_login 액티비티로 이동
+                Intent intent = new Intent(ssuchat_register.this, ssuchat_login.class);
+                startActivity(intent);
+            }
+        });
+
 
 //    public void onStart() {
 //        super.onStart();
@@ -70,24 +88,43 @@ public class ssuchat_register extends AppCompatActivity {
 //            updateUI(currentUser);
 //        }
 //    }
+    }
 
     private void signUp(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        // Check if the email is already in use
+        mAuth.fetchSignInMethodsForEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                            SignInMethodQueryResult result = task.getResult();
+                            if (result != null && result.getSignInMethods() != null && result.getSignInMethods().size() > 0) {
+                                // Email is already in use
+                                Toast.makeText(getApplicationContext(), "The account already exists for that email.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Email is not in use, proceed with user registration
+                                mAuth.createUserWithEmailAndPassword(email, password)
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                if (task.isSuccessful()) {
+                                                    // Sign up success, update UI with the signed-in user's information
+                                                    Log.d(TAG, "createUserWithEmail:success");
+                                                    FirebaseUser user = mAuth.getCurrentUser();
+                                                    updateUI(user);
+                                                } else {
+                                                    // If sign up fails, display a message to the user.
+                                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                                    Toast.makeText(getApplicationContext(), "해당 이메일의 계정이 이미 존재합니다.",
+                                                            Toast.LENGTH_SHORT).show();
+                                                    updateUI(null);
+                                                }
+                                            }
+                                        });
+                            }
                         } else {
-                            // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Log.d(TAG, "createUserWithEmail:failure");
-                            Toast.makeText(getApplicationContext(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+                            // Handle errors in fetching sign-in methods
+                            Toast.makeText(getApplicationContext(), "Error checking email existence.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
