6a4ff93fff4a93148263ff17017a9f13f2aef823
kimdoyun <roger428@naver.com>
2023-11-30 18:59:34 +0900

    Doyun Kim made

--- app/src/main/java/com/example/ssuchat/ProfessorMainPage.java
+++ app/src/main/java/com/example/ssuchat/ProfessorMainPage.java
@@ -2,15 +2,27 @@ package com.example.ssuchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.ssuchat.databinding.ActivityProfessorMainPageBinding;
 
 public class ProfessorMainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_professor_main_page);
+        ActivityProfessorMainPageBinding binding = ActivityProfessorMainPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.classAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfessorMainPage.this, ClassAddPage.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
